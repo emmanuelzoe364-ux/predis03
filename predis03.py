500d54bf8b365a02ee7ccaf78033d2b7426c2edc
emmanuelzoe364-ux <emmanuelzoe364@gmail.com>
2026-07-10 11:36:53 +0000

    Add 30m interval option and update lookback periods

--- predis03.py
+++ predis03.py
@@ -23,7 +23,7 @@ with st.sidebar:
     
     # ADDED 30m TO THE LIST
     timeframe = st.selectbox("Interval", ["1m", "5m", "15m", "30m", "1h", "4h", "1d"], index=1) 
-    lookback = st.selectbox("Data Lookback", ["1d", "3d", "7d", "1mo", "3m"], index=2)
+    lookback = st.selectbox("Data Lookback", ["1d", "3d", "7d", "1mo", "3mo"], index=2)
     
     st.divider()
     st.subheader("EMA & Stats Settings")
